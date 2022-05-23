870a1240dddb002e07d83de78cabfd206073b3f6
Niki Ito <niki.art.ito@gmail.com>
2022-05-23 07:58:49 -0400

    Add text above textInput

--- script/textInput.js
+++ script/textInput.js
@@ -1,11 +1,13 @@
 class TxtInput {
     constructor() {
-        this.input;
+        // this.input;
         // this.instructions;
-        this.submitButton;
+        // this.submitButton;
 
         this.input = createInput();
         this.input.position(windowWidth/2-this.input.width/2, windowHeight/2);
+        this.input.style("border","2px solid black");
+        this.input.style("background-color","#DEF7E2");
 
         this.submitButton = createButton('submit');
         this.submitButton.position(windowWidth/2-this.submitButton/2, windowHeight/2+this.input.height);
@@ -17,9 +19,13 @@ class TxtInput {
         // //display English text
         // textFont(minaFontB);
         // text(th
-        // this.instructions = createElement('h2', '私の名前は…。');
-        // this.instructions.position(windowWidth/2, windowHeight/8*3);
-        // this.instructions.style('text-align','center');
+        
+        this.instructions = createElement('h2', '私の名前は…。');
+        this.instructions.position(windowWidth/2-this.input.width/2, windowHeight/5*1.8);
+        this.instructions.style('font-family','wheatoncapitals');
+        this.instructions = createElement('h2', 'My name is...');
+        this.instructions.position(windowWidth/2-this.input.width/2, windowHeight/5*2);
+        this.instructions.style('font-family','mina');
 
         this.hideElements();
     }
